c5249180195c9b9615b93eb30e5312eba246ca3b
RSuganya25 <suganyar82@gmail.com>
2025-06-21 13:39:28 -0600

    Final Updates

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    url = models.URLField(blank=True)  # ✅ THIS MUST BE PRESENT
+    url = models.URLField(blank=True)  #  THIS MUST BE PRESENT
 
     def __str__(self):
         return self.title
